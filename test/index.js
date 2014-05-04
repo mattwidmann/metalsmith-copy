3febd17bbcbdcc7fe358d260d5de079849a86290
Matt Widmann <mattongit@gmail.com>
2014-05-04 16:01:21 -0400

    check error conditions and add `transform` option

--- test/index.js
+++ test/index.js
@@ -13,8 +13,8 @@ describe('metalsmith-copy', function () {
       .build(function (err) {
         if (err) return done(err);
         dir_equal('test/fixtures/simple/expected_ext', 'test/fixtures/simple/build');
-        done();
       });
+    return done();
   });
 
   it('should copy and change the directory', function (done) {
@@ -26,8 +26,8 @@ describe('metalsmith-copy', function () {
       .build(function (err) {
         if (err) return done(err);
         dir_equal('test/fixtures/simple/expected_dir', 'test/fixtures/simple/build');
-        done();
-      })
+      });
+    return done();
   })
 
   it('should copy and change both the directory and extension', function (done) {
@@ -40,8 +40,8 @@ describe('metalsmith-copy', function () {
       .build(function (err) {
         if (err) return done(err);
         dir_equal('test/fixtures/simple/expected_both', 'test/fixtures/simple/build');
-        done();
-      })
+      });
+    return done();
   })
 
   it('should not copy files not matching the pattern', function (done) {
@@ -53,8 +53,48 @@ describe('metalsmith-copy', function () {
       .build(function (err) {
         if (err) return done(err);
         dir_equal('test/fixtures/simple/expected_simple', 'test/fixtures/simple/build');
-        done();
-      })
+      });
+    return done();
+  })
+
+  it('should transform files using provided function', function (done) {
+    metalsmith('test/fixtures/simple')
+      .use(copy({
+        pattern: '*.md',
+        transform: function (file) {
+          return file + '.bak';
+        }
+      }))
+      .build(function (err) {
+        if (err) return done(err);
+        dir_equal('test/fixtures/simple/expected_transform', 'test/fixtures/simple/build');
+      });
+    return done();
+  })
+
+  it('should fail if no valid options are specified', function (done) {
+    assert.throws(function () {
+      metalsmith('test/fixtures/simple')
+      .use(copy({
+        pattern: '*.md'
+      }))
+      .build(function (err) {
+        if (err) return done(err);
+      });
+    }, Error);
+    return done();
+  })
+
+  it('should not overwrite files that already exist', function (done) {
+    metalsmith('test/fixtures/simple')
+      .use(copy({
+        pattern: '*.md',
+        extension: '.md'
+      }))
+      .build(function (err) {
+        assert(err, 'overwrote file that already existed');
+      });
+    return done();
   })
 });
 

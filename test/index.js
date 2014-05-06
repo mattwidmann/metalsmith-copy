98c72f7ac5a0d6ad80b4e04e5948e16ee3347c8c
Matt Widmann <mattongit@gmail.com>
2014-05-06 16:47:33 -0400

    refactor tests
    workaround build callback being called multiple times on errors

--- test/index.js
+++ test/index.js
@@ -4,98 +4,98 @@ var assert = require('assert'),
     collections = require('metalsmith-collections'),
     copy = require('..');
 
+function copy_test(options, fn) {
+  // build callback can be called multiple times if an error condition occurs
+  var once = false;
+
+  metalsmith('test/fixtures/simple').use(copy(options)).build(function(err, files) {
+    if (once) return;
+    once = true;
+    fn(err, files);
+  })
+}
+
 describe('metalsmith-copy', function() {
   it('should copy and change the extension', function(done) {
-    metalsmith('test/fixtures/simple').use(copy({
+    copy_test({
         pattern: '*.md',
         extension: '.text'
-      })).use(function(files, metalsmith, pluginDone) {
+      }, function(err, files) {
+        if (err) return done(err);
         assert(files['index.text'], 'file was copied');
-        pluginDone();
-      }).build(function(err) {
-        if (err) done(err)
-        else done();
+        done();
       });
   });
 
   it('should copy and change the directory', function(done) {
-    metalsmith('test/fixtures/simple').use(copy({
+    copy_test({
         pattern: '*.md',
         directory: 'out'
-      })).use(function(files, metalsmith, pluginDone) {
+      }, function(err, files) {
+        if (err) return done(err);
         assert(files['out/index.md'], 'file was copied');
-        pluginDone();
-      }).build(function(err) {
-        if (err) done(err)
-        else done();
+        done();
       });
   });
 
   it('should copy and change both the directory and extension', function(done) {
-    metalsmith('test/fixtures/simple').use(copy({
+    copy_test({
         pattern: '*.md',
         extension: '.text',
         directory: 'out'
-      })).use(function(files, metalsmith, pluginDone) {
+      }, function(err, files) {
+        if (err) return done(err);
         assert(files['out/index.text'], 'file was copied');
-        pluginDone();
-      }).build(function(err) {
-        if (err) done(err)
-        else done();
+        done();
       });
   });
 
   it('should not copy files not matching the pattern', function(done) {
-    metalsmith('test/fixtures/simple').use(copy({
+    copy_test({
         pattern: '*.mkd',
         extension: '.text'
-      })).use(function(files, metalsmith, pluginDone) {
+      }, function(err, files) {
+        if (err) return done(err);
         assert(!files['index.text'], 'file was not copied');
-        pluginDone();
-      }).build(function(err) {
-        if (err) done(err)
-        else done();
+        done();
       });
   });
 
   it('should transform files using provided function', function(done) {
-    metalsmith('test/fixtures/simple').use(copy({
+    copy_test({
         pattern: '*.md',
         transform: function(file) {
           return file + '.bak';
         }
-      })).use(function(files, metalsmith, pluginDone) {
+      }, function(err, files) {
+        if (err) return done(err);
         assert(files['index.md.bak'], 'file was copied');
-        pluginDone();
-      }).build(function(err) {
-        if (err) done(err)
-        else done();
+        done();
       });
   });
 
   describe('error handling', function() {
-    it('should fail if no valid options are specified', function(done) {
-      metalsmith('test/fixtures/simple').use(copy({
-        pattern: '*.md'
-      })).build(function(err) {
-        assert(err, 'did not fail when incorrect options were specified');
-        done();
-      });
+    it('should not overwrite files that already exist', function(done) {
+      copy_test({
+          pattern: '*.md',
+          extension: '.md'
+        }, function(err, files) {
+          if (err) return done();
+          done(new Error('overwrote file'));
+        });
     });
 
-    it('should not overwrite files that already exist', function(done) {
-      metalsmith('test/fixtures/simple').use(copy({
-        pattern: '*.md',
-        extension: '.md'
-      })).build(function(err) {
-        assert(err, 'overwrote file that already existed');
-        done();
-      });
+    it('should fail if no valid options are specified', function(done) {
+      copy_test({
+          pattern: '*.md'
+        }, function(err) {
+          assert(err, 'failed when incorrect options were specified');
+          done();
+        });
     });
 
       // if the copy was shallow, collections would mark the file with extension .md as part of the articles collection and that value would be shared to the copy, the file with extension .text, so make sure that the collection only contains 1 file if collections executes after the copy
     it('should do a deep copy of the file', function(done) {
-      done();
       var m = metalsmith('test/fixtures/simple');
 
       m.use(copy({
